a7821b0fb87c7e9a71244279185a2757e523ff93
Xscerus <xscerus@gmail.com>
2023-11-23 13:27:05 +0100

    firstCommit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        String ciag = "Długonos";
+        String wycinek1 = "";
+        String wycinek2 = "";
+        String wycinek3 = "";
+        if (ciag.length() > 2) {
+            wycinek1 = ciag.substring(ciag.length() / 2 - 1, ciag.length() / 2 + 1);
+            wycinek2 = ciag.substring(0, ciag.length() / 2 - 1);
+            wycinek3 = ciag.substring(ciag.length() / 2 + 1);
+            String add = wycinek2.concat(wycinek3);
+            String result = wycinek1.concat(add);
+            System.out.println(result);
+        }
     }
 }
